ca9ae02e212d83e48010786b37047e729deed8ab
prabha1826 <prabhakarrouthu144@gmail.com>
2024-09-27 14:16:06 +0530

    started django

--- nursery/eco/views.py
+++ nursery/eco/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    #return HttpResponse(
+    #"""
+    #<h1 style="text-align:center"> Hello Welcome to Nursery</h1>
+    #<h5>See all the Available species</h5>
+    #<p style="text-align:right">Select your likes from below</p>
+    #<img src="" alt="image">
+    #"""
+    #)
+    return render(request, "home.html")
--- nursery/nursery/urls.py
+++ nursery/nursery/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from eco.views import home
 urlpatterns = [
+    path('',home,name="home"),
     path('admin/', admin.site.urls),
 ]
